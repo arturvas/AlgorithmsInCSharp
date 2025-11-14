c7daa9516135196473bf76894ce887101db85863
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-11-14 13:30:19 -0400

    Add binary search implementation in Program.cs

--- Chapter01_BinarySearch/Program.cs
+++ Chapter01_BinarySearch/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("--- Binary Search ---");
+return;
 
-Console.WriteLine("Hello, World!");
+int BinarySearch(int[] array, int value)
+{
+    var baixo = 0;
+    var alto = array.Length - 1;
+
+    while (baixo <= alto)
+    {
+        var meio = (baixo + alto) / 2;
+        var chute = array[meio];
+
+        if (chute == value)
+        {
+            return meio;
+        }
+        if (chute > value)
+        {
+            alto = meio - 1;
+        }
+        else
+        {
+            baixo = meio + 1;
+        }
+    }
+
+    return 0;
+}
